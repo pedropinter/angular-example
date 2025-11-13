2d551663fa02ebe172a5109b381417a252ade3b3
pedropinter <pedrohenripinter0@gmail.com>
2025-11-13 17:15:54 -0300

    erronoterminal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CalendarComponent} from "@schedule-x/angular";
-import {createCalendar, viewWeek} from "@schedule-x/calendar";
-import '@schedule-x/theme-default/dist/calendar.css'
-import {createEventModalPlugin} from "@schedule-x/event-modal";
-import {createDragAndDropPlugin} from "@schedule-x/drag-and-drop";
-
+import { CalendarComponent } from "@schedule-x/angular";
+import { createCalendar, viewWeek,createViewWeek  } from "@schedule-x/calendar";
+import '@schedule-x/theme-default/dist/calendar.css';
+import { createEventModalPlugin } from "@schedule-x/event-modal";
+import { createDragAndDropPlugin } from "@schedule-x/drag-and-drop";
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CalendarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']   
 })
 export class AppComponent {
   title = 'angular-example';
@@ -19,12 +18,12 @@ export class AppComponent {
     events: [
       {
         id: '1',
-        title: 'Event 1',
-        start: '2024-06-11 03:00',
-        end: '2024-06-11 05:00',
+        title: 'Portugues',
+        start: '2025-11-11 01:00',
+        end: '2025-11-11 02:00',
       },
     ],
-    views: [viewWeek],
+    views: [viewWeek,createViewWeek()],
     plugins: [createEventModalPlugin(), createDragAndDropPlugin()]
   })
 }
